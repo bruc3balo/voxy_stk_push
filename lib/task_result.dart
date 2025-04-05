cfb53e6789de102bcd2948e05271738d4065b4ca
bruc3balo <brucelosis12@gmail.com>
2025-04-05 23:13:41 +0300

    print succes

--- lib/task_result.dart
+++ lib/task_result.dart
@@ -4,9 +4,15 @@ sealed class TaskResult<T> {}
 class Success<T> extends TaskResult<T> {
   final T data;
   Success(this.data);
+
+  @override
+  String toString() => "$data";
 }
 
 class Error<T> extends TaskResult<T> {
   final Exception errorMessage;
   Error(this.errorMessage);
+
+  @override
+  String toString() => "$errorMessage";
 }
